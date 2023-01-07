57cb790e06584ecc6885c6e1edf89de1c0c5d4c9
ShobanaBohs100 <letter2shobana@gmail.com>
2023-01-07 15:18:46 +0530

    Updated page config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `shobanathangaraj.com`,
+    title: `www.shobanabohs.com`,
     siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
